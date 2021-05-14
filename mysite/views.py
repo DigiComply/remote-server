b00547e26fb4d464a587aa0d7f5a355615853166
Pranjal Kushwaha <pranjal0819@gmail.com>
2021-05-14 14:09:27 +0530

    Add replit support

--- mysite/views.py
+++ mysite/views.py
@@ -1,30 +1,31 @@
-import requests
 import json
 
-from django.views.decorators.csrf import csrf_exempt
+import requests
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 @csrf_exempt
 def index(request):
-  print('Welcome')
-  
-  if request.method == 'POST':
-    data = json.loads(request.body)
-    url = data.get('url')
-    headers = data.get('headers')
-    method = data.get('method')
-    timeout = int(data.get('timeout', 0))
-    data = data.get('data')
+    print('Welcome')
 
-    try:
-      if method == 'GET':
-        res = requests.get(url=url, headers=headers, timeout=timeout)
-        return HttpResponse(res.content, status=res.status_code)
-      elif method == 'POST':
-        res = requests.post(url=url, headers=headers, timeout=timeout, data=data)
-        return HttpResponse(res.content, status=res.status_code)
-    
-    except Exception as e:
-      return HttpResponse(e, status=500)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        url = data.get('url')
+        headers = data.get('headers')
+        method = data.get('method')
+        timeout = int(data.get('timeout', 0))
+        data = data.get('data')
 
-  return HttpResponse("OK", status=200)
+        try:
+            if method == 'GET':
+                res = requests.get(url=url, headers=headers, timeout=timeout)
+                return HttpResponse(res.content, status=res.status_code)
+            elif method == 'POST':
+                res = requests.post(url=url, headers=headers, timeout=timeout, data=data)
+                return HttpResponse(res.content, status=res.status_code)
+
+        except Exception as e:
+            return HttpResponse(e, status=500)
+
+    return HttpResponse("OK", status=200)
